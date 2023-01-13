e513fbb859450af61adb12c0737a98a8f53f875a
Joseph DeCommer <jdecommer@gmail.com>
2023-01-13 14:58:07 -0500

    Fixed output

--- SortingAlgorithms/MergeSort.java
+++ SortingAlgorithms/MergeSort.java
@@ -6,53 +6,76 @@ public class MergeSort {
     * modpoint = (start + end) / 2
     * Not in place (needs additional temporary arrays)
     * O(nlogn) (base 2) Time complexity
-    * Stable
+    * Stable algorithm
     *
-    * The mergeSort method will not print output and I dont know why.
     */
-    public static void main(String[] args) {
+   
+        public static void main(String args[]){  
+        int a[] = { 19, 30, 79, -7, 31, 2, 39, -42 };  
+        int n = a.length;  
+        MergeSort m1 = new MergeSort();  
+        System.out.println("\nBefore sorting array elements are - ");  
+        m1.printArray(a, n);  
+        m1.mergeSort(a, 0, n - 1);  
+        System.out.println("\nAfter sorting array elements are - ");  
+        m1.printArray(a, n);  
+        System.out.println("");  
+    }
 
-        int[] intArray = { 20, 43, -19, 7, 92, 1, -79 };
-
-        mergeSort(intArray, 0, 0);
+    /* Function to merge the subarrays of a[] */  
+    void merge(int a[], int beg, int mid, int end)    
+    {    
+        int i, j, k;  
+        int n1 = mid - beg + 1;    
+        int n2 = end - mid;    
         
-        for(int i = 0; i < intArray.length; i++) {
-            mergeSort(intArray, 0, intArray.length);
-        }
-/*         for(int i = 0; i < intArray.length; i++) {
-            System.out.print(intArray[i] + " ");
-        } */
-       
-    }
-
-    public static void mergeSort(int[] input, int start, int end) {
-        if (end - start < 2) {
-            return;
-        } else {
-            int mid = (start + end) / 2; //partitions the array
-            mergeSort(input, start, mid); //recursive call that stars the party with left array
-            mergeSort(input, mid, end); // deals with right array
-            merge(input, start, mid, end);
-        }
-    }
-
-    public static void merge(int[] input, int start, int mid, int end) { // merges sorted partitioned arrays
-
-        if (input[mid - 1] <= input[mid]) { //checks if left and right values are sorted
-            return;
-        }
-        int i = start;
-        int j = mid;
-        int tempIndex = 0;
-
-        int[] temp = new int[end - start];
-
-        while(i < mid && j < end) {
-            temp[tempIndex++] = input[i] <= input[j] ? input[i++] : input[j++]; 
-        }
-
-        System.arraycopy(input, i, input, start + tempIndex, mid - i);
-        System.arraycopy(temp, 0, input, start, tempIndex);
-
-    }
+    /* temporary Arrays */  
+        int LeftArray[] = new int[n1];  
+        int RightArray[] = new int[n2];  
+        
+        /* copy data to temp arrays */  
+        for (i = 0; i < n1; i++) LeftArray[i] = a[beg + i];    
+        for (j = 0; j < n2; j++) RightArray[j] = a[mid + 1 + j];    
+        
+        i = 0; /* initial index of first sub-array */  
+        j = 0; /* initial index of second sub-array */   
+        k = beg;  /* initial index of merged sub-array */  
+        
+        while (i < n1 && j < n2) {    
+            if(LeftArray[i] <= RightArray[j]) {    
+                a[k] = LeftArray[i];    
+                i++;    
+            }else {    
+                a[k] = RightArray[j];    
+                j++;    
+            }    
+            k++;    
+        }    
+        while (i<n1){    
+            a[k] = LeftArray[i];    
+            i++;    
+            k++;    
+        }    
+        while (j<n2){    
+            a[k] = RightArray[j];    
+            j++;    
+            k++;    
+        }    
+    }    
+    
+    void mergeSort(int a[], int beg, int end) {  
+        if (beg < end){  
+            int mid = (beg + end) / 2;  
+            mergeSort(a, beg, mid);  
+            mergeSort(a, mid + 1, end);  
+            merge(a, beg, mid, end);  
+        }  
+    }  
+    
+    /* Function to print the array */  
+    void printArray(int a[], int n) {  
+        int i;  
+        for (i = 0; i < n; i++)  
+            System.out.print(a[i] + " ");  
+    }  
 }
