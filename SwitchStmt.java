c6595ea4903c8c07a3f24b1fe6df86b00fa4fbe2
Joseph DeCommer <jdecommer@gmail.com>
2023-01-17 16:49:43 -0500

    Added Switch.java file

---
+++ SwitchStmt.java
@@ -0,0 +1,23 @@
+public class SwitchStmt {
+
+    public static void main(String[] args) {
+ 
+        String[] names = {"Joseph", "Constance", "Spoons", "Shit"};
+ 
+        int likes = names.length;
+
+        switch (likes) {
+            case 0:
+                System.out.println("no one likes this");
+            case 1:
+                System.out.println(String.format("%s likes this", names[0]));
+            case 2:
+                System.out.println(String.format("%s and %s like this", names[0], names[1]));
+            case 3:
+                System.out.println(String.format("%s, %s and %s like this", names[0], names[1], names[2]));
+            default:
+                System.out.println(String.format("%s, %s and %d others like this", names[0], names[1], likes-2));
+        }
+ 
+    }    
+}
