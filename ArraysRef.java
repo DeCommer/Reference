0cee5912ffc89a22701f5526b2c42294efbc90b4
Joseph DeCommer <jdecommer@gmail.com>
2023-01-24 19:14:38 -0500

    additions

--- ArraysRef.java
+++ ArraysRef.java
@@ -1,24 +1,25 @@
+// import java.util.Arrays;
+
 public class ArraysRef {
     public static void main(String[] args) {
-        int[] intArray = new int[7]; //cannot change size after being set
+        int[] intArray = new int[3]; //cannot change size after being set
+            intArray[0] = 20;
+            intArray[1] = 35;
+            intArray[2] = -15;
 
-        intArray[0] = 20;
-        intArray[1] = 35;
-        intArray[2] = -15;
-        intArray[3] = 7;
-        intArray[4] = 55;
-        intArray[5] = 1;
-        intArray[6] = -22;
+            String strArr[] = {"Word", "Word", "Word"};
 
         int index = -1;
         for(int i=0; i < intArray.length; i++) {
-            if(intArray[i] == 7) {
+            if(intArray[i] == 20) {
                 index = i;
                 break;
             }
         }
-
         System.out.println("Index = " + index);
+
+        //Array methods
+
 
     }
 }
