baeb4d8e6a66e50c4b8cabf22f3d1deee07f8d6d
Joseph DeCommer <jdecommer@gmail.com>
2023-02-02 08:33:48 -0500

    added total of combinations to output

--- CombiPermu.java
+++ CombiPermu.java
@@ -4,20 +4,23 @@ public class CombiPermu {
      * Each k-combination of a set S of n elements has k! permutations
      * 
     */
-
     //permutations = nPr = n! / (n - r)!
     
     public static void main(String[] args) 
     {
         //Print all permutations of a string
-        String sampleString = "joe";
+        String sampleString = "123";
         int n = sampleString.length();
+        int r = 3;
         CombiPermu permutation = new CombiPermu();
+        int numOf = factorial(n)/factorial(n - r);
         permutation.permute(sampleString, 0, n - 1);
-
+        System.out.println("The number of combinations is: " + numOf);
     }
+
     public void permute(String str, int start, int end) 
     {
+        
         if(start == end)
             System.out.println(str);
             else
@@ -30,6 +33,7 @@ public class CombiPermu {
                 }
             }
     }
+
     public static String swap(String a, int i, int j)
     {
         char temp;
@@ -40,7 +44,7 @@ public class CombiPermu {
         return String.valueOf(charArr);
     }
 
-    public int factorial(int num) 
+    public static int factorial(int num) 
     {
         if(num == 0) 
         {
