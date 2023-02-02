7e850297be6ac953495fdce11b867b5896eebb41
Joseph DeCommer <jdecommer@gmail.com>
2023-02-02 14:45:12 -0500

    daily parctice

--- GuessingGame.java
+++ GuessingGame.java
@@ -2,29 +2,33 @@ import java.util.Scanner;
 public class GuessingGame {
 
     public static void main(String[] args) {
-        int secretNum = (int)Math.random()*100;
+        int secretNum = (int)Math.floor(Math.random()*10) + 1;
         boolean condition = false;
-        int numOfGuesses = 0;
+        int numOfGuesses = 1;
         Scanner input = new Scanner(System.in);
 
+        System.out.println("The secret numbers is: " + secretNum);
 
-        while(condition == false) {
+        while(condition == false)
+        {
 
             System.out.print("Try to guess my number, filthy human: ");
             int answer = input.nextInt();
 
-            if(answer == secretNum) {
-                System.out.println("\nYou got it right, damn dirty human!");
-                System.out.println("It took you " + numOfGuesses + " guesses! Haha!");
+            if(answer == secretNum) 
+            {
+                System.out.println("\nYou got it right, in " + numOfGuesses + " guesses. Damn dirty human!");
                 System.out.println("Good bye.");
                 condition = true;
-            } else {
+            } else 
+            {
                 System.out.println("Keep trying smelly human.");
                 condition = false;
                 numOfGuesses++;
-                System.out.println("Nuber of guesses: " + numOfGuesses);
+                System.out.println("Number of guesses: " + numOfGuesses);
             }
         }
         input.close();
+
     }
 }
