071bdf68701aefe0118479622d88328bf86afb62
Joseph DeCommer <jdecommer@gmail.com>
2023-03-13 10:45:00 -0400

    -

--- Algorithms.java
+++ Algorithms.java
@@ -3,10 +3,16 @@ public class Algorithms {
     public static void main(String[] args) {
         int a[] = {10, 20, 30, 40, 50, 60, 70, 80, 90};
 
-        int searchElement = 800;
+        int searchElement = 50;
+
+        searchArr(a, searchElement);
+        
+    }
+
+    public static void searchArr(int[] a, int searchElement) {
         boolean flag = false;
 
-        for(int i = 0; i < a.length - 1; i++) {
+        for(int i = 0; i < a.length -1; i++) {
             if(searchElement == a[i]){
                 System.out.println("Element found at index: " + i);
                 flag = true;
