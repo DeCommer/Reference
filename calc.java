7a7b8fd036df3c64b363ea3460aac6464aade8ed
Joseph DeCommer <jdecommer@gmail.com>
2023-03-20 13:30:15 -0400

    practice

---
+++ calc.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class calc {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int num1;
+        int num2;
+
+
+        
+    }
+
+}
