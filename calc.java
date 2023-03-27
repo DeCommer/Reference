e19bf79f9e06c659aa1d00b26dc26a7b630ad924
Joseph DeCommer <jdecommer@gmail.com>
2023-03-27 12:43:30 -0400

    simple practice

--- calc.java
+++ calc.java
@@ -5,11 +5,18 @@ public class calc {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int num1;
-        int num2;
 
+        System.out.print("Enter first number: ");
+        int num1 = input.nextInt();
 
-        
+        System.out.print("Enter second number: ");
+        int num2 = input.nextInt();
+
+        int answer = num1 + num2;
+
+        System.out.println("Number 1 plus number 2 = " + answer );
+
+        input.close();
     }
 
 }
