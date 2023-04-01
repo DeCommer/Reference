ebfd51de0099911ceeac9e784bb1b96b2b15ca77
Joseph DeCommer <jdecommer@gmail.com>
2023-04-01 17:59:45 -0400

    added Venus stats

--- Main.java
+++ Main.java
@@ -4,10 +4,14 @@ public class Main {
     public static void main(String[] args) {
 
         DemoClass planet = new DemoClass("Earth", 150_000_000L, 60_000_000_000L, 12756l, 24);
+        DemoClass planet2 = new DemoClass("Venus", 660_827_000L, 186_000_000_000_000L, 7520, 243);
         
 
         System.out.println("The planet " + planet.getName() + 
         " is approximately " + planet.getDistFromSun() + "km from the Sun");
+
+        System.out.println("The planet " + planet2.getName() + 
+        " is approximately " + planet2.getDistFromSun() + "km from the Sun");
 
     }
 }
