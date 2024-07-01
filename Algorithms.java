ad277f029dabde2c7939e8068a84fc30ac62dff7
Joseph DeCommer <jdecommer@gmail.com>
2024-07-01 06:26:39 -0400

    practice

--- Algorithms.java
+++ Algorithms.java
@@ -1,33 +1,37 @@
+import java.util.Arrays;
+
 public class Algorithms {
     //Basic array search
     public static void main(String[] args) {
-        // int arr[] = {10, 20, 30, 40, 50, 60, 70, 80, 90};
+        int arr[] = {10, 20, 30, 40, 50, 60, 70, 80, 90};
 
-        // int searchElement = 50;
+        int searchElement = 50;
 
-        // searchArr(arr, searchElement);
+        searchArr(arr, searchElement);
 
-        String greet = "hi";
+        // String greet = "hi";
 
-        for(int i = 0; i <= 10; i ++) {
-            System.out.println(greet);
-        }
+        // for(int i = 0; i <= 10; i ++) {
+        //     System.out.println(greet);
+        // }
         
     }
 
-    // public static void searchArr(int[] arr, int searchElement) {
-    //     boolean flag = false;
+    public static void searchArr(int[] arr, int searchElement) {
 
-    //     for(int i = 0; i < arr.length -1; i++) {
-    //         if(searchElement == arr[i]){
-    //             System.out.println("Element " + arr[i] + " is found at index: " + i);
-    //             flag = true;
-    //             break;
-    //         } else if(flag == false){
-    //             System.out.println("Not found");
-    //             break;
-    //         }
-    //     }
-    // }
+        String stArr = Arrays.toString(arr);
+        System.out.println(searchElement);
+        System.out.println(stArr);
+
+        for(int i = 0; i < arr.length; i++) {
+            if(searchElement == arr[i]){
+                System.out.println("Element " + arr[i] + " is found at index: " + i);
+                break;
+            } else if(searchElement != arr[i]){
+                System.out.println(i);
+                break;
+            }
+        }
+    }
 }
 
